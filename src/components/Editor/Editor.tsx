89850b2b2e423b32b33d8590c536fff2ee13cca2
童一川 <3327158223@qq.com>
2022-10-05 22:09:50 +0800

    feat: 简易更换主题

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -6,19 +6,37 @@ import EditorFooter from './EditorFooter'
 import _EditorTest from './_EditorTest'
 
 import MarkCore from '@/core'
+import { ElButton } from 'element-plus'
 
 export default defineComponent({
   setup() {
     const editor = ref<HTMLDivElement | null>(null)
     onMounted(() => {
       console.log(editor.value)
-
       const mark = new MarkCore(editor?.value, {})
       mark.init()
+      const edit = document.getElementsByClassName(styles.editor)[0]
+      const basic = document.getElementsByClassName(styles.input)[0]
+      const button = document.querySelectorAll('.button')
+      button[0].onclick = () => {
+        console.log(button)
+        console.log(basic.style.color)
+        if (basic.style.color !== 'black') {
+          basic.style.color = 'black'
+          basic.style.backgroundColor = 'white'
+          edit.style.backgroundColor = 'grey'
+        } else {
+          basic.style.color = 'white'
+          basic.style.backgroundColor = 'grey'
+          edit.style.backgroundColor = 'white'
+        }
+      }
     })
+
     return () => (
       <div class={styles.root}>
         <EditorHeader />
+        <ElButton class="button">更换主题</ElButton>
         <div class={styles.editor}>
           <div class={styles.input} ref={editor}></div>
         </div>
