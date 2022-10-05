f63166d63da6448cbe4f10366361acc6efeaf0f0
zhouchangsi <=>
2022-10-05 20:03:18 +0800

    test: test: 测试core

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -1,18 +1,26 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
 import styles from './Editor.module.scss'
 
 import EditorHeader from './EditorHeader'
 import EditorFooter from './EditorFooter'
 import _EditorTest from './_EditorTest'
+
+import MarkCore from '@/core'
+
 export default defineComponent({
   setup() {
+    const editor = ref<HTMLDivElement | null>(null)
+    onMounted(() => {
+      console.log(editor.value)
+
+      const mark = new MarkCore(editor?.value, {})
+      mark.init()
+    })
     return () => (
       <div class={styles.root}>
         <EditorHeader />
         <div class={styles.editor}>
-          <div class={styles.input}>
-            <_EditorTest />
-          </div>
+          <div class={styles.input} ref={editor}></div>
         </div>
         <EditorFooter />
       </div>
