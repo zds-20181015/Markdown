e40d22e2e6a0256b96e0726f1df488bedbdfa647
mafqla <1692285748@qq.com>
2022-10-08 14:44:23 +0800

    fix: 去除多余的包

--- src/utils/useMarkCore.ts
+++ src/utils/useMarkCore.ts
@@ -1,19 +1,3 @@
-import MarkCore from '@/core'
-
-/**
- * 确保markcore只有一个实例,输入container创建新实例，否则返回旧实例
- */
-let mark: MarkCore | undefined
-export function useMarkCore(container?: HTMLDivElement): MarkCore | undefined {
-  if (container) {
-    mark = new MarkCore(container, {})
-    mark.init()
-  } else if (mark === undefined) {
-    throw Error('未初始化')
-  }
-  return mark
-}
-
 import Muya from '@/lib'
 import EmojiPicker from '@/lib/ui/emojiPicker'
 import FormatPicker from '@/lib/ui/formatPicker'
