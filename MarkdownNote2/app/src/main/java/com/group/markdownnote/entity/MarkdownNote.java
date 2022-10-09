95d5193b638bacd47bbee25c51bb573ea217c255
zds-20181015 <44169575+zds-20181015@users.noreply.github.com>
2022-10-09 11:59:07 +0800

    更新设计

--- MarkdownNote2/app/src/main/java/com/group/markdownnote/entity/MarkdownNote.java
+++ MarkdownNote2/app/src/main/java/com/group/markdownnote/entity/MarkdownNote.java
@@ -1,113 +1,96 @@
 package com.group.markdownnote.entity;
 
-import java.text.SimpleDateFormat;
+import androidx.annotation.NonNull;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 public class MarkdownNote {
-  private int id;
-  private String content;
-  private Date date;
-  private String title;
-  private List<String> tags = new ArrayList<String>();
+    private int id;
+    private String content;
+    private Date date;
+    private String title;
+    private List<String> tags = new ArrayList<>();
 
-  public MarkdownNote(int id, Date date, String content, String title, String tag) {
-      this.id = id;
-      this.content = content;
-      this.date = date;
-      this.title = title;
-      setTags(tag);
-  }
-
-  public String getContent() {
-    return content;
-  }
-
-  public void setContent(String content) {
-    this.content = content;
-  }
-
-  public String getTitle() {
-    if (title == null) {
-      return "TITLE";
+    public MarkdownNote(int id, Date date, String content, String title, String tags) {
+        this.id = id;
+        this.date = date;
+        this.content = content;
+        this.title = title;
+        setTags(tags);
     }
-    return title;
-  }
 
-  public void setTitle(String title) {
-    title = title.replace("\n", "");
-    this.title = title;
-  }
-
-  public Date getDate() {
-    return date;
-  }
-
-  public int getId() {
-    return id;
-  }
-
-  public String getFormattedTags() {
-    if (tags == null) return "";
-    StringBuilder tg = new StringBuilder();
-    for (int i = 0; i < tags.size(); i++) {
-      tg.append(tags.get(i)).append(",");
+    public int getId() {
+        return id;
     }
-    if (tg.length() < 1) return "";
-    return tg.substring(0, tg.length() - 1);
-  }
 
-  public void setTags(String str) {
-    if (str != null) {
-      String[] res = str.split(",");
-      tags.addAll(Arrays.asList(res));
+    public String getContent() {
+        return content;
     }
-  }
 
-  public List<String> getTags() {
-    return tags;
-  }
+    public void setContent(String content) {
+        this.content = content;
+    }
 
-  public String getShortContent() {
-    String rv = "";
-    if (content == null || content.length() < 100) rv = content;
-    else rv = content.substring(0, 100) + "...";;
-    return rv;
-  }
+    public Date getDate() {
+        return date;
+    }
 
-  public String getShortTitle() {
-    String rv = "";
-    if (title == null || title.length() < 10) rv = getTitle();
-    else rv = getTitle().substring(0, 8) + "...";
-    return rv;
-  }
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
-  public String getDay() {
-    SimpleDateFormat sdf = new SimpleDateFormat("dd");
-    return sdf.format(date);
-  }
+    public String getTitle() {
+        return title;
+    }
 
-  public String getMonth() {
-    SimpleDateFormat sdf = new SimpleDateFormat("MMM", Locale.US);
-    return sdf.format(date);
-  }
+    public void setTitle(String title) {
+        title = title.replace("\n", "");
+        this.title = title;
+    }
 
-  public String getYear() {
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy");
-    return sdf.format(date);
-  }
+    public List<String> getTags() {
+        return tags;
+    }
 
-  @Override
-  public String toString() {
-    return "MarkdownNote{" +
-            "id=" + id +
-            ", content='" + content + '\'' +
-            ", date=" + date +
-            ", title='" + title + '\'' +
-            ", tags=" + tags +
-            '}';
-  }
+    public void setTags(String str) {
+        if (str != null) {
+            String[] str_array = str.split(",");
+            tags.addAll(Arrays.asList(str_array));
+        }
+    }
+
+    public String getShortContent() {
+        String str = "";
+        if (content == null || content.length() < 100) {
+            str = content;
+        } else {
+            str = content.substring(0, 100) + "...";
+        };
+        return str;
+    }
+
+    public String getShortTitle() {
+        String str = "";
+        if (title == null || title.length() < 10) {
+            str = getTitle();
+        } else {
+            str = getTitle().substring(0, 8) + "...";
+        }
+        return str;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "MarkdownNote{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", date=" + date +
+                ", title='" + title + '\'' +
+                ", tags=" + tags +
+                '}';
+    }
 }
