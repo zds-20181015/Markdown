e03b353e69c800fa9629a2b233fd440517d21cc0
zds-20181015 <44169575+zds-20181015@users.noreply.github.com>
2022-10-09 11:48:52 +0800

    优化Markdown语法显示
    迭代Markdown编辑器基本功能

--- MarkdownNote2/app/src/main/java/com/group/markdownnote/AddMarkdownNoteActivity.java
+++ MarkdownNote2/app/src/main/java/com/group/markdownnote/AddMarkdownNoteActivity.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.util.Log;
 import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.webkit.*;
+import android.widget.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -19,9 +22,11 @@ import com.group.markdownnote.utils.ToastUtils;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class AddMarkdownNoteActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = AddMarkdownNoteActivity.class.getName();
+    public static AddMarkdownNoteActivity instance;
     private MyApp mApp;
     private ArrayList<MarkdownNote> noteArrayList;
     private int mId;
@@ -31,25 +36,34 @@ public class AddMarkdownNoteActivity extends AppCompatActivity implements View.O
     private EditText et_title;
     private EditText et_content;
 
+    private boolean isSource;
+
     private ImageButton bt_preview, bt_format, bt_listb, bt_listn, bt_link, bt_quote;
     private ImageButton bt_codeb, bt_bold, bt_bk, bt_image, bt_camera, bt_italic;
     private Button bt_txt, bt_h1, bt_h2, bt_h3, bt_h4, bt_h5, bt_h6;
     private WebView wv_markdown_note;
 
+    @SuppressLint({"SetJavaScriptEnabled", "AddJavascriptInterface", "JavascriptInterface"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_markdown_note);
 
         mApp = (MyApp) getApplication();
-        noteArrayList = mApp.getNoteArrayList();
+
         mId = mApp.getId();
+        instance = AddMarkdownNoteActivity.this;
 
         initUI();
         initListener();
         et_content.setVisibility(View.VISIBLE);
 
+        noteArrayList = mApp.getNoteArrayList();
+        isSource = true;
+
         wv_markdown_note.loadUrl("file:///android_asset/index.html");
+        wv_markdown_note.getSettings().setJavaScriptEnabled(true);
+        wv_markdown_note.addJavascriptInterface(new JsInterface(), "androidJS");
     }
 
     private void initUI() {
@@ -110,6 +124,7 @@ public class AddMarkdownNoteActivity extends AppCompatActivity implements View.O
         bt_h6.setOnClickListener(this);
 
         wv_markdown_note.setOnClickListener(this);
+
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -146,6 +161,72 @@ public class AddMarkdownNoteActivity extends AppCompatActivity implements View.O
 
                 sendToJS("**Text**");
             }
+            case R.id.bt_bk:{
+
+                sendToJS("~~Text~~");
+            }
+            case R.id.bt_italic:{
+
+                sendToJS("*Text*");
+            }
+
+
+            case R.id.bt_preview:{
+
+//                preview();
+            }
+            case R.id.bt_format:{
+
+
+            }
+            case R.id.bt_listb:{
+//
+//                sendToJS("- Text");
+            }
+            case R.id.bt_listn:{
+//
+//                sendToJS("1. Text");
+            }
+            case R.id.bt_link:{
+//
+//                sendToJS("[Alt](https://)");
+            }
+            case R.id.bt_quote:{
+
+                sendToJS("> Text");
+            }
+            case R.id.bt_codeb:{
+
+                sendToJS("```Text```");
+
+
+            }case R.id.bt_txt:{
+
+//                editSource();
+            }
+            case R.id.bt_h1:{
+
+                sendToJS("# Text");
+            }
+            case R.id.bt_h2:{
+
+                sendToJS("## Text");
+            }
+            case R.id.bt_h3:{
+
+                sendToJS("### Text");
+            }
+            case R.id.bt_h4:{
+
+                sendToJS("#### Text");
+            }case R.id.bt_h5:{
+
+                sendToJS("##### Text");
+            }case R.id.bt_h6:{
+
+                sendToJS("###### Text");
+            }
+
 
             default:
                 break;
@@ -154,7 +235,7 @@ public class AddMarkdownNoteActivity extends AppCompatActivity implements View.O
 
     public void sendToJS(String msg) {
         if (msg == null) return;
-        if (true) {
+        if (isSource) {
             int index = et_content.getSelectionStart();
             Editable editable = et_content.getText();
             editable.insert(index, msg);
@@ -162,6 +243,79 @@ public class AddMarkdownNoteActivity extends AppCompatActivity implements View.O
             msg = convert(msg);
             wv_markdown_note.loadUrl("javascript:insert(" + "\"" + msg + "\"" + ");");
         }
+    }
+
+    interface Callback {
+        void onResult(String value);
+    };
+
+    public void arti(Callback callback) {
+        wv_markdown_note.evaluateJavascript(
+                "javascript:getArticle();",
+                new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String s) {
+                        s = format(s);    // 返回JS值
+                        callback.onResult(s);
+                    }
+                });
+    }
+
+
+    public void setup(String msg) {
+        msg = convert(msg);
+        msg = format(msg);
+        wv_markdown_note.loadUrl("javascript:msetup(" + "\"" + msg + "\"" + ");");
+        Log.d("SETUP", "SETUP CALLED");
+    }
+
+    public void editSource() {
+        if (isSource) {
+            wv_markdown_note.setVisibility(View.VISIBLE);
+            setup(et_content.getText().toString());
+            et_content.setVisibility(View.GONE);
+            wv_markdown_note.requestFocus();
+            wv_markdown_note.requestFocusFromTouch();
+            isSource = false;
+        } else {
+            arti(
+                    new Callback() {
+                        @Override
+                        public void onResult(String value) {
+                            wv_markdown_note.setVisibility(View.GONE);
+                            et_content.setVisibility(View.VISIBLE);
+                            et_content.setText(value);
+                            et_content.requestFocus();
+                            et_content.requestFocusFromTouch();
+                            isSource = true;
+                        }
+                    });
+        }
+    }
+
+    public void preview() {
+        wv_markdown_note.loadUrl("javascript:preview();");
+    }
+
+    public static class JsInterface {
+        JsInterface() {}
+
+        @SuppressLint("JavascriptInterface")
+        @JavascriptInterface
+        public void setValue(List<String> ls, int index) {
+//            new MarkdownNote(AddMarkdownNoteActivity.instance, ls, index);
+//            MarkdownNote note = new MarkdownNote(AddMarkdownNoteActivity.instance, ls, index);
+        }
+    }
+
+    public static String format(String unicode) {
+        unicode = unicode.replace("\\n", "\n");
+        unicode = unicode.replace("\"", "");
+        //        unicode = unicode.replace("\\\\","");
+        unicode = unicode.replace("\\u003C", "<");
+        String regex = "<[^>]*>";
+        unicode = unicode.replaceAll(regex, "");
+        return unicode;
     }
 
     public static String convert(String string) {
